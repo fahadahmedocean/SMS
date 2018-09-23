72b09ae7b0b85d0ac2e5b1e348c4778403bc7eeb
Fahad Ahmed <33807051+fahadahmedocean@users.noreply.github.com>
2018-09-23 19:22:18 +0600

    Update Utils.java

--- src/main/java/com/mbstu/ict/Utils.java
+++ src/main/java/com/mbstu/ict/Utils.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
- * Created by habib on 1/7/18.
+ * Created by fahad on 1/7/18.
  */
 public class Utils {
 
